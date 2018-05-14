d5e54e39d0794b46f683cbdad6865fd469be4fc1
Miguel <12604047+RatserX@users.noreply.github.com>
2018-05-14 01:46:34 -0500

    TaxForce: Updated logos, stylesheets, php, html, js

---
+++ taxforce/assets/js/index.js
@@ -0,0 +1,11 @@
+var index = {
+    form: {
+        send: function (element, event) {
+            event.preventDefault();
+
+            var form = element.form;
+
+            return false;
+        }
+    }
+}
