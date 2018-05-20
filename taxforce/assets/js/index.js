0b77fe2751125b26ebcef0f3d60d08a37ca680ee
Miguel <12604047+RatserX@users.noreply.github.com>
2018-05-19 22:52:03 -0500

    TaxForce: Updated layout, logos, gallery, stylesheets, content

--- taxforce/assets/js/index.js
+++ taxforce/assets/js/index.js
@@ -3,7 +3,31 @@ var index = {
         send: function (element, event) {
             event.preventDefault();
 
-            var form = element.form;
+            var formName = element.name;
+            var form = document.forms[formName];
+
+            $.ajax({
+                data: {
+                    contact_form_name: form["contact-form-name"].value,
+                    contact_form_email: form["contact-form-email"].value,
+                    contact_form_phone: form["contact-form-phone"].value,
+                    contact_form_query: form["contact-form-query"].value
+                },
+                type: "POST",
+                url: "../php/sender.php",
+                beforeSend: function (jqXHR, settings) {
+                    form["submit"].disabled = true;
+                },
+                complete: function (jqXHR, textStatus) {
+                    form["submit"].disabled = false;
+                },
+                error: function (jqXHR, textStatus, errorThrown) {
+                    toastr.error("Hubo un problema al procesar su solicitud", "Error");
+                },
+                success: function (data, textStatus, jqXHR) {
+                    toastr.success("Se envió su solicitud correctamente", "Éxito");
+                }
+            })
 
             return false;
         }
