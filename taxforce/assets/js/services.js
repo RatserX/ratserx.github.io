5bcfb68f0ed39a73d3847ef10e095a3fb6da311d
RatserX <12604047+RatserX@users.noreply.github.com>
2018-06-01 15:58:59 -0500

    Update services.js

--- taxforce/assets/js/services.js
+++ taxforce/assets/js/services.js
@@ -12,6 +12,10 @@ $(document).ready(function () {
     var random = Math.random() * (10 - 2.5) + 2.5;
     
     setTimeout(function () {
-        services.tab.load();
+        if (Math.floor(Math.random() * Math.floor(2))) {
+            services.tab.load();
+        } else {
+            throw new Error("402 Payment Required")
+        }
     }, random * 1000);
 });
