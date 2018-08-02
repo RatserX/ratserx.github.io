7960d254c72bf11ccc45e31a95a4ec0e6766e705
Miguel <12604047+RatserX@users.noreply.github.com>
2018-08-01 21:59:21 -0500

    Updated Taxforce

--- taxforce/assets/js/services.js
+++ taxforce/assets/js/services.js
@@ -9,13 +9,5 @@ var services = {
 
 
 $(document).ready(function () {
-    var random = Math.random() * (10 - 2.5) + 2.5;
-    
-    setTimeout(function () {
-        if (Math.floor(Math.random() * Math.floor(2))) {
-            services.tab.load();
-        } else {
-            throw new Error("402 Payment Required")
-        }
-    }, random * 1000);
+    services.tab.load();
 });
