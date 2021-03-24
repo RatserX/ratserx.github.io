26efa28d88a1441191a62fb267a45bd30b99e732
Miguel <12604047+RatserX@users.noreply.github.com>
2021-03-23 20:49:09 -0500

    fix(unica-decrypt): dpi, nsp text

--- unica-decrypt/index.js
+++ unica-decrypt/index.js
@@ -2,7 +2,21 @@ const listener = () => {
     ["input"].forEach((type) => {
         document.getElementById("encrypted-grade").addEventListener(type, (event) => {
             const encryptedGrade = event.target.value;
-            document.getElementById("decrypted-grade").value = decryptGrade(encryptedGrade);
+            const decryptedGrade = decryptGrade(encryptedGrade);
+
+            switch (decryptedGrade)
+            {
+                case -2:
+                    decryptedGrade = "DPI";
+
+                    break;
+                case -1:
+                    decryptedGrade = "NSP";
+
+                    break;
+            }
+
+            document.getElementById("decrypted-grade").value = decryptedGrade;
         })
     })
 }
